de426c2fb2e3ccfeac381089f7534a7fc508c027
Adam Graham <adam.zigurous@gmail.com>
2017-02-10 21:15:26 -0600

    Unit test interpolate func of InterpolationValues

--- STween/STweenTests/Interpolation/InterpolationValuesTest.swift
+++ STween/STweenTests/Interpolation/InterpolationValuesTest.swift
@@ -21,4 +21,11 @@ class InterpolationValuesTest: XCTestCase {
         XCTAssertEqual(reversed.end, 0.0)
     }
 
+    func testInterpolate() {
+        let values: InterpolationValues<Double> = InterpolationValues(start: 0.0, end: 1.0)
+        let interpolatedValueA = values.interpolate(with: .linear, elapsed: 0.5, duration: 1.0)
+        let interpolatedValueB = Double.interpolate(from: 0.0, to: 1.0, with: .linear, elapsed: 0.5, duration: 1.0)
+        XCTAssertEqual(interpolatedValueA, interpolatedValueB)
+    }
+
 }
