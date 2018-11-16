6123a758ad8a915e3f3b116ba932312643769a47
Adam Graham <adam.zigurous@gmail.com>
2018-11-16 16:30:29 -0600

    Convert Ease to a struct that stores properties

--- STween/STween/Easing/Ease.swift
+++ STween/STween/Easing/Ease.swift
@@ -8,206 +8,216 @@
 
 /// A type of easing function, described from an algorithmic classification and acceleration
 /// pattern.
-public enum Ease {
+public struct Ease {
+
+    /// The algorithmic classification of the ease.
+    public let classification: EaseClassification
+    /// The acceleration pattern of the ease.
+    public let curve: EaseCurve
+
+    /// :nodoc:
+    private init(classification: EaseClassification, curve: EaseCurve) {
+        self.classification = classification
+        self.curve = curve
+    }
+
+}
+
+extension Ease: Equatable {
+
+    public static func ==(lhs: Ease, rhs: Ease) -> Bool {
+        return lhs.classification == rhs.classification &&
+            lhs.curve == rhs.curve
+    }
+
+}
+
+// MARK: -
+
+extension Ease {
 
     // MARK: Linear
 
     /// An ease with a `linear` classification.
     ///
     /// [Visual Reference](http://easings.net/)
-    case linear
+    public static let linear = Ease(classification: .linear, curve: .none)
 
     // MARK: Sinusoidal
 
     /// An ease with a `sine` classification and `in` acceleration pattern.
     ///
     /// [Visual Reference](http://easings.net/#easeInSine)
-    case sineIn
+    public static let sineIn = Ease(classification: .sine, curve: .in)
 
     /// An ease with a `sine` classification and `out` acceleration pattern.
     ///
     /// [Visual Reference](http://easings.net/#easeOutSine)
-    case sineOut
+    public static let sineOut = Ease(classification: .sine, curve: .out)
 
     /// An ease with a `sine` classification and `inOut` acceleration pattern.
     ///
     /// [Visual Reference](http://easings.net/#easeInOutSine)
-    case sineInOut
+    public static let sineInOut = Ease(classification: .sine, curve: .inOut)
 
     // MARK: Cubic
 
     /// An ease with a `cubic` classification and `in` acceleration pattern.
     ///
     /// [Visual Reference](http://easings.net/#easeInCubic)
-    case cubicIn
+    public static let cubicIn = Ease(classification: .cubic, curve: .in)
 
     /// An ease with a `cubic` classification and `out` acceleration pattern.
     ///
     /// [Visual Reference](http://easings.net/#easeOutCubic)
-    case cubicOut
+    public static let cubicOut = Ease(classification: .cubic, curve: .out)
 
     /// An ease with a `cubic` classification and `inOut` acceleration pattern.
     ///
     /// [Visual Reference](http://easings.net/#easeInOutCubic)
-    case cubicInOut
+    public static let cubicInOut = Ease(classification: .cubic, curve: .inOut)
 
     // MARK: Quadratic
 
     /// An ease with a `quad` classification and `in` acceleration pattern.
     ///
     /// [Visual Reference](http://easings.net/#easeInQuad)
-    case quadIn
+    public static let quadIn = Ease(classification: .quad, curve: .in)
 
     /// An ease with a `quad` classification and `out` acceleration pattern.
     ///
     /// [Visual Reference](http://easings.net/#easeOutQuad)
-    case quadOut
+    public static let quadOut = Ease(classification: .quad, curve: .out)
 
     /// An ease with a `quad` classification and `inOut` acceleration pattern.
     ///
     /// [Visual Reference](http://easings.net/#easeInOutQuad)
-    case quadInOut
+    public static let quadInOut = Ease(classification: .quad, curve: .inOut)
 
     // MARK: Quartic
 
     /// An ease with a `quart` classification and `in` acceleration pattern.
     ///
     /// [Visual Reference](http://easings.net/#easeInQuart)
-    case quartIn
+    public static let quartIn = Ease(classification: .quart, curve: .in)
 
     /// An ease with a `quart` classification and `out` acceleration pattern.
     ///
     /// [Visual Reference](http://easings.net/#easeOutQuart)
-    case quartOut
+    public static let quartOut = Ease(classification: .quart, curve: .out)
 
     /// An ease with a `quart` classification and `inOut` acceleration pattern.
     ///
     /// [Visual Reference](http://easings.net/#easeInOutQuart)
-    case quartInOut
+    public static let quartInOut = Ease(classification: .quart, curve: .inOut)
 
     // MARK: Quintic
 
     /// An ease with a `quint` classification and `in` acceleration pattern.
     ///
     /// [Visual Reference](http://easings.net/#easeInQuint)
-    case quintIn
+    public static let quintIn = Ease(classification: .quint, curve: .in)
 
     /// An ease with a `quint` classification and `out` acceleration pattern.
     ///
     /// [Visual Reference](http://easings.net/#easeOutQuint)
-    case quintOut
+    public static let quintOut = Ease(classification: .quint, curve: .out)
 
     /// An ease with a `quint` classification and `inOut` acceleration pattern.
     ///
     /// [Visual Reference](http://easings.net/#easeInOutQuint)
-    case quintInOut
+    public static let quintInOut = Ease(classification: .quint, curve: .inOut)
 
     // MARK: Exponential
 
-    /// An ease with a `expo` classification and `in` acceleration pattern.
+    /// An ease with an `expo` classification and `in` acceleration pattern.
     ///
     /// [Visual Reference](http://easings.net/#easeInExpo)
-    case expoIn
+    public static let expoIn = Ease(classification: .expo, curve: .in)
 
-    /// An ease with a `expo` classification and `out` acceleration pattern.
+    /// An ease with an `expo` classification and `out` acceleration pattern.
     ///
     /// [Visual Reference](http://easings.net/#easeOutExpo)
-    case expoOut
+    public static let expoOut = Ease(classification: .expo, curve: .out)
 
-    /// An ease with a `expo` classification and `inOut` acceleration pattern.
+    /// An ease with an `expo` classification and `inOut` acceleration pattern.
     ///
     /// [Visual Reference](http://easings.net/#easeInOutExpo)
-    case expoInOut
+    public static let expoInOut = Ease(classification: .expo, curve: .inOut)
 
     // MARK: Circular
 
     /// An ease with a `circ` classification and `in` acceleration pattern.
     ///
     /// [Visual Reference](http://easings.net/#easeInCirc)
-    case circIn
+    public static let circIn = Ease(classification: .circ, curve: .in)
 
     /// An ease with a `circ` classification and `out` acceleration pattern.
     ///
     /// [Visual Reference](http://easings.net/#easeOutCirc)
-    case circOut
+    public static let circOut = Ease(classification: .circ, curve: .out)
 
     /// An ease with a `circ` classification and `inOut` acceleration pattern.
     ///
     /// [Visual Reference](http://easings.net/#easeInOutCirc)
-    case circInOut
+    public static let circInOut = Ease(classification: .circ, curve: .inOut)
 
     // MARK: Back
 
     /// An ease with a `back` classification and `in` acceleration pattern.
     ///
     /// [Visual Reference](http://easings.net/#easeInBack)
-    case backIn
+    public static let backIn = Ease(classification: .back, curve: .in)
 
     /// An ease with a `back` classification and `out` acceleration pattern.
     ///
     /// [Visual Reference](http://easings.net/#easeOutBack)
-    case backOut
+    public static let backOut = Ease(classification: .back, curve: .out)
 
     /// An ease with a `back` classification and `inOut` acceleration pattern.
     ///
     /// [Visual Reference](http://easings.net/#easeInOutBack)
-    case backInOut
+    public static let backInOut = Ease(classification: .back, curve: .inOut)
 
     // MARK: Elastic
 
-    /// An ease with a `elastic` classification and `in` acceleration pattern.
+    /// An ease with an `elastic` classification and `in` acceleration pattern.
     ///
     /// [Visual Reference](http://easings.net/#easeInElastic)
-    case elasticIn
+    public static let elasticIn = Ease(classification: .elastic, curve: .in)
 
-    /// An ease with a `elastic` classification and `out` acceleration pattern.
+    /// An ease with an `elastic` classification and `out` acceleration pattern.
     ///
     /// [Visual Reference](http://easings.net/#easeOutElastic)
-    case elasticOut
+    public static let elasticOut = Ease(classification: .elastic, curve: .out)
 
-    /// An ease with a `elastic` classification and `inOut` acceleration pattern.
+    /// An ease with an `elastic` classification and `inOut` acceleration pattern.
     ///
     /// [Visual Reference](http://easings.net/#easeInOutElastic)
-    case elasticInOut
+    public static let elasticInOut = Ease(classification: .elastic, curve: .inOut)
 
     // MARK: Bounce
 
     /// An ease with a `bounce` classification and `in` acceleration pattern.
     ///
     /// [Visual Reference](http://easings.net/#easeInBounce)
-    case bounceIn
+    public static let bounceIn = Ease(classification: .bounce, curve: .in)
 
     /// An ease with a `bounce` classification and `out` acceleration pattern.
     ///
     /// [Visual Reference](http://easings.net/#easeOutBounce)
-    case bounceOut
+    public static let bounceOut = Ease(classification: .bounce, curve: .out)
 
     /// An ease with a `bounce` classification and `inOut` acceleration pattern.
     ///
     /// [Visual Reference](http://easings.net/#easeInOutBounce)
-    case bounceInOut
+    public static let bounceInOut = Ease(classification: .bounce, curve: .inOut)
 
 }
 
-extension Ease {
-
-    // MARK: Properties
-
-    /// The classification of the ease.
-    public var classification: EaseClassification {
-        return EaseMappings.eases[self]!.classification
-    }
-
-    /// The acceleration pattern of the ease.
-    public var curve: EaseCurve {
-        return EaseMappings.eases[self]!.curve
-    }
-    
-}
+// MARK: -
 
 extension Ease {
-
-    // MARK: Methods
 
     /**
      Calculates the value between a start and end value at a specific point in time.
@@ -230,9 +240,6 @@ extension Ease {
         let d = duration
 
         switch self {
-        case .linear:
-            return EaseFunction.linear(b: b, c: c, t: t, d: d)
-
         case .sineIn:
             return EaseFunction.sineIn(b: b, c: c, t: t, d: d)
         case .sineOut:
@@ -302,6 +309,9 @@ extension Ease {
             return EaseFunction.bounceOut(b: b, c: c, t: t, d: d)
         case .bounceInOut:
             return EaseFunction.bounceInOut(b: b, c: c, t: t, d: d)
+
+        default:
+            return EaseFunction.linear(b: b, c: c, t: t, d: d)
         }
     }
 
