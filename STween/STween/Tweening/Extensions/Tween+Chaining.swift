d98e91da3eae67765f8d5916118ca0b9446827c7
Adam Graham <adam.zigurous@gmail.com>
2019-06-06 23:42:22 -0500

    Change method chaining naming convention

--- STween/STween/Tweening/Extensions/Tween+Chaining.swift
+++ STween/STween/Tweening/Extensions/Tween+Chaining.swift
@@ -14,9 +14,9 @@ import Foundation
 
  ```
  view.tween(to: .width(100.0), duration: 1.0)
-    .set(ease: .backOut)
-    .set(delay: 0.5)
-    .set(onComplete: { ... })
+    .ease(.backOut)
+    .delay(0.5)
+    .onComplete({ ... })
  ```
  */
 extension Tween {
@@ -24,170 +24,170 @@ extension Tween {
     // MARK: Chaining Methods
 
     /**
-     Assigns a value to the `ease` property of the tween.
+     A method chain to assign a value to the `ease` property of the tween.
      
      - Parameters:
-        - ease: The `Ease` value to be assigned.
+        - value: The `Ease` value to be assigned.
     
      - Returns: The tween to allow for additional method chaining.
      */
-    @discardableResult public func set(ease: Ease) -> Tween {
-        self.ease = ease
+    @discardableResult public func ease(_ value: Ease) -> Tween {
+        self.ease = value
         return self
     }
 
     /**
-     Assigns a value to the `delay` property of the tween.
+     A method chain to assign a value to the `delay` property of the tween.
 
      - Parameters:
-        - delay: The amount of seconds to be assigned.
+        - value: The amount of seconds to be assigned.
 
      - Returns: The tween to allow for additional method chaining.
      */
-    @discardableResult public func set(delay: TimeInterval) -> Tween {
-        self.delay = delay
+    @discardableResult public func delay(_ value: TimeInterval) -> Tween {
+        self.delay = value
         return self
     }
 
     /**
-     Assigns a value to the `duration` property of the tween.
+     A method chain to assign a value to the `duration` property of the tween.
 
      - Parameters:
-        - duration: The amount of seconds to be assigned.
+        - value: The amount of seconds to be assigned.
 
      - Returns: The tween to allow for additional method chaining.
      */
-    @discardableResult public func set(duration: TimeInterval) -> Tween {
-        self.duration = duration
+    @discardableResult public func duration(_ value: TimeInterval) -> Tween {
+        self.duration = value
         return self
     }
 
     /**
-     Assigns a value to the `reversed` property of the tween.
+     A method chain to assign a value to the `reversed` property of the tween.
 
      - Parameters:
-        - reversed: The boolean value to be assigned.
+        - value: The boolean value to be assigned.
 
      - Returns: The tween to allow for additional method chaining.
      */
-    @discardableResult public func set(reversed: Bool) -> Tween {
-        self.reversed = reversed
+    @discardableResult public func reversed(_ value: Bool) -> Tween {
+        self.reversed = value
         return self
     }
 
     /**
-     Assigns a callback to the `onUpdate` property of the tween.
+     A method chain to assign a callback to the `onUpdate` property of the tween.
 
      - Parameters:
         - callback: The callback closure to be assigned.
 
      - Returns: The tween to allow for additional method chaining.
      */
-    @discardableResult public func set(onUpdate callback: @escaping Callback) -> Tween {
+    @discardableResult public func onUpdate(_ callback: @escaping Callback) -> Tween {
         self.onUpdate = callback
         return self
     }
 
     /**
-     Assigns a callback to the `onStart` property of the tween.
+     A method chain to assign a callback to the `onStart` property of the tween.
 
      - Parameters:
         - callback: The callback closure to be assigned.
 
      - Returns: The tween to allow for additional method chaining.
      */
-    @discardableResult public func set(onStart callback: @escaping Callback) -> Tween {
+    @discardableResult public func onStart(_ callback: @escaping Callback) -> Tween {
         self.onStart = callback
         return self
     }
 
     /**
-     Assigns a callback to the `onStop` property of the tween.
+     A method chain to assign a callback to the `onStop` property of the tween.
 
      - Parameters:
         - callback: The callback closure to be assigned.
 
      - Returns: The tween to allow for additional method chaining.
      */
-    @discardableResult public func set(onStop callback: @escaping Callback) -> Tween {
+    @discardableResult public func onStop(_ callback: @escaping Callback) -> Tween {
         self.onStop = callback
         return self
     }
 
     /**
-     Assigns a callback to the `onRestart` property of the tween.
+     A method chain to assign a callback to the `onRestart` property of the tween.
 
      - Parameters:
         - callback: The callback closure to be assigned.
 
      - Returns: The tween to allow for additional method chaining.
      */
-    @discardableResult public func set(onRestart callback: @escaping Callback) -> Tween {
+    @discardableResult public func onRestart(_ callback: @escaping Callback) -> Tween {
         self.onRestart = callback
         return self
     }
 
     /**
-     Assigns a callback to the `onPause` property of the tween.
+     A method chain to assign a callback to the `onPause` property of the tween.
 
      - Parameters:
         - callback: The callback closure to be assigned.
 
      - Returns: The tween to allow for additional method chaining.
      */
-    @discardableResult public func set(onPause callback: @escaping Callback) -> Tween {
+    @discardableResult public func onPause(_ callback: @escaping Callback) -> Tween {
         self.onPause = callback
         return self
     }
 
     /**
-     Assigns a callback to the `onResume` property of the tween.
+     A method chain to assign a callback to the `onResume` property of the tween.
 
      - Parameters:
         - callback: The callback closure to be assigned.
 
      - Returns: The tween to allow for additional method chaining.
      */
-    @discardableResult public func set(onResume callback: @escaping Callback) -> Tween {
+    @discardableResult public func onResume(_ callback: @escaping Callback) -> Tween {
         self.onResume = callback
         return self
     }
 
     /**
-     Assigns a callback to the `onComplete` property of the tween.
+     A method chain to assign a callback to the `onComplete` property of the tween.
 
      - Parameters:
         - callback: The callback closure to be assigned.
 
      - Returns: The tween to allow for additional method chaining.
      */
-    @discardableResult public func set(onComplete callback: @escaping Callback) -> Tween {
+    @discardableResult public func onComplete(_ callback: @escaping Callback) -> Tween {
         self.onComplete = callback
         return self
     }
 
     /**
-     Assigns a callback to the `onKill` property of the tween.
+     A method chain to assign a callback to the `onKill` property of the tween.
 
      - Parameters:
         - callback: The callback closure to be assigned.
 
      - Returns: The tween to allow for additional method chaining.
      */
-    @discardableResult public func set(onKill callback: @escaping Callback) -> Tween {
+    @discardableResult public func onKill(_ callback: @escaping Callback) -> Tween {
         self.onKill = callback
         return self
     }
 
     /**
-     Assigns a callback to the `onRevive` property of the tween.
+     A method chain to assign a callback to the `onRevive` property of the tween.
 
      - Parameters:
         - callback: The callback closure to be assigned.
 
      - Returns: The tween to allow for additional method chaining.
      */
-    @discardableResult public func set(onRevive callback: @escaping Callback) -> Tween {
+    @discardableResult public func onRevive(_ callback: @escaping Callback) -> Tween {
         self.onRevive = callback
         return self
     }
--- STween/STweenTests/Tweening/Extensions/Tween+ChainingTest.swift
+++ STween/STweenTests/Tweening/Extensions/Tween+ChainingTest.swift
@@ -24,79 +24,79 @@ class Tween_ChainingTest: XCTestCase {
 
     func testSetEase() {
         let tween = Tweener.default.animate(UIView(), to: [UIViewTweenProperty](), duration: 1.0)
-            .set(ease: .backOut)
+            .ease(.backOut)
         XCTAssertEqual(tween.ease, .backOut)
     }
 
     func testSetDelay() {
         let tween = Tweener.default.animate(UIView(), to: [UIViewTweenProperty](), duration: 1.0)
-            .set(delay: 1.0)
+            .delay(1.0)
         XCTAssertEqual(tween.delay, 1.0)
     }
 
     func testSetDuration() {
         let tween = Tweener.default.animate(UIView(), to: [UIViewTweenProperty](), duration: 1.0)
-            .set(duration: 2.0)
+            .duration(2.0)
         XCTAssertEqual(tween.duration, 2.0)
     }
 
     func testSetReversed() {
         let tween = Tweener.default.animate(UIView(), to: [UIViewTweenProperty](), duration: 1.0)
-            .set(reversed: true)
+            .reversed(true)
         XCTAssertTrue(tween.reversed)
     }
 
     func testSetOnUpdate() {
         let tween = Tweener.default.animate(UIView(), to: [UIViewTweenProperty](), duration: 1.0)
-            .set(onUpdate: { _ in })
+            .onUpdate({ _ in })
         XCTAssertNotNil(tween.onUpdate)
     }
 
     func testSetOnStart() {
         let tween = Tweener.default.animate(UIView(), to: [UIViewTweenProperty](), duration: 1.0)
-            .set(onStart: { _ in })
+            .onStart({ _ in })
         XCTAssertNotNil(tween.onStart)
     }
 
     func testSetOnStop() {
         let tween = Tweener.default.animate(UIView(), to: [UIViewTweenProperty](), duration: 1.0)
-            .set(onStop: { _ in })
+            .onStop({ _ in })
         XCTAssertNotNil(tween.onStop)
     }
 
     func testSetOnRestart() {
         let tween = Tweener.default.animate(UIView(), to: [UIViewTweenProperty](), duration: 1.0)
-            .set(onRestart: { _ in })
+            .onRestart({ _ in })
         XCTAssertNotNil(tween.onRestart)
     }
 
     func testSetOnPause() {
         let tween = Tweener.default.animate(UIView(), to: [UIViewTweenProperty](), duration: 1.0)
-            .set(onPause: { _ in })
+            .onPause({ _ in })
         XCTAssertNotNil(tween.onPause)
     }
 
     func testSetOnResume() {
         let tween = Tweener.default.animate(UIView(), to: [UIViewTweenProperty](), duration: 1.0)
-            .set(onResume: { _ in })
+            .onResume({ _ in })
         XCTAssertNotNil(tween.onResume)
     }
 
     func testSetOnComplete() {
         let tween = Tweener.default.animate(UIView(), to: [UIViewTweenProperty](), duration: 1.0)
-            .set(onComplete: { _ in })
+            .onComplete({ _ in })
         XCTAssertNotNil(tween.onComplete)
     }
 
     func testSetOnKill() {
         let tween = Tweener.default.animate(UIView(), to: [UIViewTweenProperty](), duration: 1.0)
-            .set(onKill: { _ in })
+            .onKill({ _ in })
         XCTAssertNotNil(tween.onKill)
     }
 
     func testSetOnRevive() {
         let tween = Tweener.default.animate(UIView(), to: [UIViewTweenProperty](), duration: 1.0)
-            .set(onRevive: { _ in })
+            .onRevive({ _ in })
         XCTAssertNotNil(tween.onRevive)
     }
 
